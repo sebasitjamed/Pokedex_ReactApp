97ebc449c2d3a7a539109349bf8da37779556a66
Sebastian Sitja <sebasitjamed@gmail.com>
2020-01-04 17:41:54 -0300

    Updates

---
+++ precache-manifest.9af5ea1967840aa507300a981e2fd786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d56040b645558f97fe072963966ef19",
+    "url": "/Pokedex_ReactApp/index.html"
+  },
+  {
+    "revision": "28c8687357dd16d46048",
+    "url": "/Pokedex_ReactApp/static/css/main.b194903b.chunk.css"
+  },
+  {
+    "revision": "f2d2fa7669cc3c140f4f",
+    "url": "/Pokedex_ReactApp/static/js/2.40529310.chunk.js"
+  },
+  {
+    "revision": "b846902a03bddff09b754d87bdd08a3c",
+    "url": "/Pokedex_ReactApp/static/js/2.40529310.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28c8687357dd16d46048",
+    "url": "/Pokedex_ReactApp/static/js/main.ea5b8953.chunk.js"
+  },
+  {
+    "revision": "cc0c811c147518cff3fd",
+    "url": "/Pokedex_ReactApp/static/js/runtime-main.7e713ee6.js"
+  },
+  {
+    "revision": "de8a0f82f1449b48f71828ede116ecd9",
+    "url": "/Pokedex_ReactApp/static/media/pokelogo.de8a0f82.png"
+  }
+]);
